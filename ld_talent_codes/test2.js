0ecee92db95b625c7957bf80af3e3f74da640212
sidahmed <ahorob57@gmail.com>
2021-06-29 15:09:53 +0100

    added the examenOcclusionDynamic to paro/prothese

---
+++ ld_talent_codes/test2.js
@@ -0,0 +1,47 @@
+// CODE 2
+/*
+Write a function that takes as input a filename. 
+The function parses the file reading line by line and determines if
+the line contains a single valid number. 
+It should return a list of all the numbers found in the file. 
+
+Example input file:
+==========================================
+#This is an input file with some text and numbers
+10
+  -50
+Some more numbers follow
+3.1457
+12  00 47
+1.25E-7
+-20000
+==========================================
+
+Above function should return [10, -50, 3.1457, 1.25E-7, -20000]
+*/
+
+const fs = require('fs')
+function readFileSync(pathToFile) {
+    try {
+        const data = fs.readFileSync(pathToFile, 'utf8')
+        const textContent = data.toString()
+
+        return textContent
+    } catch (e) {
+        throw Error(e.stack)
+    }
+}
+
+const pathToFile = './test2.file.txt' // make sure you substitute 'pathToFile' with the actual path to a file in your filesystem.
+const fileText = readFileSync(pathToFile)
+
+function getListOfAllNumbersFromText(text) {
+    // regular expression to sreach for a valid number
+    const regex = RegExp(/\d[\w]/, 'g')
+    const listOfNumbers = text.match(regex)
+
+    return listOfNumbers
+}
+
+let result = getListOfAllNumbersFromText(fileText)
+console.log(result) // [10, -50, 3.1457, 1.25E-7, -20000]
--- models/prothese.js
+++ models/prothese.js
@@ -75,5 +75,29 @@ export default [
                 -accept: "application/json"
                 -contentType: "application/json"
         `
+    },
+    {
+        title: 'examenOcclusionDynamic collection',
+        method: 'POST',
+        route: '/odf/examenOcclusionDynamic',
+        request: `
+            (
+                -protrution_cote travaillaint: "string" @< rapport2/4 >
+                -protrution_conte non travaillaint: "string" @< desoclusion_posterieure, interference >
+                -lateralite_droit_cote_travaillaint: "string" @< foction_canine, fonction_groupe >
+                -lateralite_droit_non_travaillaint: "string" @< desocclusion, interference >
+                -lateralite_gauche_cote_travaillaint: "string" @< foction canine, fonction_groupe >
+                -lateralite_gauche_non_travaillaint: "string" @< desocclusion, interference >
+            )
+        `,
+        response: `
+            {}
+        `,
+        headers: `
+            *Request Headers*
+                -authToken: "HS256_bearer_token" !
+                -accept: "application/json"
+                -contentType: "application/json"
+        `
     }
 ]
