5aa10b2e936e8affe189e4885537ad3a12fb04fb
amine_mohamed <amineminatoo2@gmali.com>
2021-06-30 16:23:53 +0200

    added the paro model and maldie ar resque collections

---
+++ models/pcb.js
@@ -0,0 +1,122 @@
+export default [
+    {
+        title: 'maladie_risque_cardiopathie collection',
+        method: 'POST',
+        route: '/pcb/maladie_risque/cardiopathie',
+        request: `
+            -type_cardiopathie: "string" @< >,
+            -pathologie_associe: "string" @< >,
+            -risque_endocardite: "string" @< >,
+            -risque_endocardite: "string" @< >,
+            -traitement_anticoagulante: "string" @< >,
+            -traitement_cours: "string" @< >,
+            -affection_associees: "string" @< >,
+            -examen_complementaires_demande: "string" @< >,
+            -decision_therapeutique: "string" @< >,
+            -protocole_antibiothique: "string" @< >,
+            -observation_particulieres: "string" @< >,
+     
+        `,
+        response: ``
+    },
+    {
+        title: 'maladie_risque_diabete collection',
+        method: 'POST',
+        route: '/pcb/maladie_risque/diabete',
+        request: `
+            -type_diabete: "string" @< type1, type2 >,
+            -pathologie_associe: "string" @< >,
+            -traitement_cours: "string" @< >,
+            -malaise_hypoglycemique: "string" @< >,
+            -glycemie: "string" @< >,
+            -hemoglobinegyque: "string" @< >,
+            -examen_complementaires_demande: "string" @< >,
+            -decision_therapeutique: "string" @< >,
+            -equilibre_diabete: "string" @< >,
+            -protocole_antibiothique: "string" @< >,
+            -observation_particulieres: "string" @< >,
+            
+        `,
+        response: ``
+    },
+    {
+        title: 'maladie_risque_bisphosphonates collection',
+        method: 'POST',
+        route: '/pcb/maladie_risque/bisphosphonates',
+        request: `
+            -diagnostique_maladie : "string" @< type1, type2 >,
+            -pathologie_associe: "string" @< >,
+            -traitement_bisphosphonate_type: "string" @< >,
+            -traitement_bisphosphonate_dose: "string" @< >,
+            -traitement_bisphosphonate_voie: "string" @< >,
+            -traitement_bisphosphonate_nombre_cures: "string" @< >,
+            -traitement_bisphosphonate_type_date_debute_taitement: "string" @< >,
+            -traitement_bisphosphonate_type_date_fin_taitement: "string" @< >,
+            -traitement associes:: "string" @< >,
+            -examen_complementaires_demande: "string" @< >,
+            -decision_therapeutique: "string" @< >,
+            -protocole_antibiothique: "string" @< >,
+            -observation_particulieres: "string" @< >,
+        
+            
+        `,
+        response: ``
+    },
+    {
+        title: 'maladie_risque_hemopathie collection',
+        method: 'POST',
+        route: '/pcb/maladie_risque/hemopathie',
+        request: `
+            -diagnostique_maladie: "string" @< type1, type2 >,
+            -pathologie_associe: "string" @< >,
+            -antecedentes_saignemet : "string" @< >,
+            -antecedentes_saignemet_familialle: "string" @< >,
+            -traitement_cours: "string" @< >,
+            -groupe_sanguin: "string" @< >,
+            -examen_complementaires_demande: "string" @< >,
+            -decision_therapeutique: "string" @< >,
+            -transfusion_sanguine : "string" @< >,
+            -adjoction_facteure_manquant : "string" @< >
+            -protocole_antibiothique: "string" @< >,
+            -observation_particulieres: "string" @< >,
+
+        `,
+        response: ``
+    },
+    {
+        title: 'maladie_risque_irradie collection',
+        method: 'POST',
+        route: '/pcb/maladie_risque/irradie',
+        request: `
+            -diagnostique_maladie : "string" @< type1, type2 >,
+            -pathologie_associe: "string" @< >,
+            -traitement_cours: "string" @< >,
+            -traitement_dose_iradiation: "string" @< >,
+            -traitement_type_date_debute_taitement: "string" @< >,
+            -traitement_type_date_fin_taitement: "string" @< >,
+            -traitement associes: "string" @< >,
+            -complication_poste_radiotherapie: "string" @< >,
+            -examen_complementaires_demande: "string" @< >,
+            -decision_therapeutique: "string" @< >,
+            -protocole_antibiothique: "string" @< >,
+            -observation_particulieres: "string" @< >,
+        `,
+        response: ``
+    },
+    {
+        title: 'maladie_risque_accident cellulaire collection',
+        method: 'POST',
+        route: '/pcb/maladie_risque/accident cellulaire',
+        request: `
+            -date_appraition : "string" @< type1, type2 >,
+            -pathologie_associe: "string" @< >,
+            -traitement_cours: "string" @< >,
+            -examen_complementaires_demande: "string" @< >,
+            -diagnostic_positive: "string" @< >,
+            -diagnostique_etiologique: "string" @< >,
+            -protocole_antibiothique: "string" @< >,
+            observation_particulieres: "string" @< >,
+        `,
+        response: ``
+    }
+]
