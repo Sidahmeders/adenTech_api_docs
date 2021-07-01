8255a32171911728ae0b7db5b9c31df5c662fd85
sidahmed <ahorob57@gmail.com>
2021-07-01 18:47:43 +0100

    added the oce ongoing treament of the oce

--- models/treatments.js
+++ models/treatments.js
@@ -1,1 +1,12 @@
-export default [{}]
+export default [
+    {
+        title: 'ongoing treatment OCE',
+        method: 'POST',
+        route: '/ongoing-treatments/oce',
+        request: `
+            -tooth-number: "integer" ! @< 11, 12, ..., 47, 47 >,
+            -
+        `,
+        response: ``
+    }
+]
