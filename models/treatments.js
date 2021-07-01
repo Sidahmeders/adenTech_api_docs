01d766530f889dcc54878cc014533d87cc0a590c
sidahmed <ahorob57@gmail.com>
2021-07-01 19:07:22 +0100

    added all the ongoing treatments collections

--- models/treatments.js
+++ models/treatments.js
@@ -4,9 +4,105 @@ export default [
         method: 'POST',
         route: '/ongoing-treatments/oce',
         request: `
-            -tooth-number: "integer" ! @< 11, 12, ..., 47, 47 >,
-            -decision terapeutique: "string" ! @< dentinogene, cementogene, osteocementogene >,
+            -tooth_number: "integer" ! @< 11, 12, ..., 47, 47 >,
+            -decision_terapeutique: "string" ! @< dentinogene, cementogene, osteocementogene >
         `,
-        response: ``
+        response: ``,
+        headers: `
+        *Request Headers*
+            -authToken: "HS256_bearer_token" !
+            -accept: "application/json"
+            -contentType: "application/json"
+        `,
+        specification: `
+            *decision_terapeutique*
+            -dentinogene: ,
+            -cementogene: ,
+            -osteocementogene: ,
+        `
+    },
+    {
+        title: 'ongoing treatment ODF',
+        method: 'POST',
+        route: '/ongoing-treatments/odf',
+        request: `
+            -decision_terapeutique: "string" ! @< >
+        `,
+        response: ``,
+        headers: `
+        *Request Headers*
+            -authToken: "HS256_bearer_token" !
+            -accept: "application/json"
+            -contentType: "application/json"
+        `,
+        specification: `
+            *decision_terapeutique*
+            -
+            -
+            -
+        `
+    },
+    {
+        title: 'ongoing treatment PARO',
+        method: 'POST',
+        route: '/ongoing-treatments/paro',
+        request: `
+            -decision_terapeutique: "string" ! @< >
+        `,
+        response: ``,
+        headers: `
+        *Request Headers*
+            -authToken: "HS256_bearer_token" !
+            -accept: "application/json"
+            -contentType: "application/json"
+        `,
+        specification: `
+            *decision_terapeutique*
+            -
+            -
+            -
+        `
+    },
+    {
+        title: 'ongoing treatment prothese',
+        method: 'POST',
+        route: '/ongoing-treatments/prothese',
+        request: `
+            -decision_terapeutique: "string" ! @< >
+        `,
+        response: ``,
+        headers: `
+        *Request Headers*
+            -authToken: "HS256_bearer_token" !
+            -accept: "application/json"
+            -contentType: "application/json"
+        `,
+        specification: `
+            *decision_terapeutique*
+            -
+            -
+            -
+        `
+    },
+    {
+        title: 'ongoing treatment PCB',
+        method: 'POST',
+        route: '/ongoing-treatments/paro',
+        request: `
+            -decision_terapeutique: "string" ! @< >
+        `,
+        response: ``,
+        headers: `
+        *Request Headers*
+            -authToken: "HS256_bearer_token" !
+            -accept: "application/json"
+            -contentType: "application/json"
+        `,
+        specification: `
+            *decision_terapeutique*
+            -
+            -
+            -
+        `
     }
 ]
