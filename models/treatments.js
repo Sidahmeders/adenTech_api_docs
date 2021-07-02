eac534a92c503baeca31210d223aa9797f668f4d
amine_mohamed <amineminatoo2@gmali.com>
2021-07-02 03:04:24 +0200

    adde some new specification to the ongoing treatment OCE collection

--- models/treatments.js
+++ models/treatments.js
@@ -16,9 +16,13 @@ export default [
         `,
         specification: `
             *decision_terapeutique*
-            -dentinogene: 
-            -cementogene: 
-            -osteocementogene: 
+                -dentinogene, cementogene, osteocementogene: @< traitement_urgence, pancements_serie, radiographie_retro_aleolaire_, blanchimente_dente_vivante,
+                    radiographie_panoramique,  blanchimente_dente_devitalise, coiffage_pulpaire_inderecte, coiffage_pulpaire_derecte, pulpotomie, traitemente_endo_ dent_ temporaire,
+                    apexogenese, apexification, depulpation_mono_radiculaire, depulpation_bi_radiculaire, depulpation_molaires, depulpation_dds, retraitement_endo_mono,
+                    retraitement_endo_pluri, restauration_amalgame_01face, restauration_amalgame_02face, restauration_amalgame_03face, reconstitution_amalgame_large_avec_ancrage,
+                    restauration_simple_composite, reconstitution_large_composite_avec_ancrage, scellement_couronne, scellement_bridge, detartrage, 
+                    curtage_periapecale, exerese_kyste_petit_volume, exerese_kyste_grand_volume, amputation_radiculaire, finition_polissage,
+                >
         `
     },
     {
