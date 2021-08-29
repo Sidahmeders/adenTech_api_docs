757e09eb3e0b3cb5198ded3cc5a1d18413c9a26c
Sidahmeders <ahorob57@gmail.com>
2021-08-29 19:46:34 +0200

    fixed some small mistakes in genral-models

--- models/general.js
+++ models/general.js
@@ -36,7 +36,7 @@ export default [
                 -patient_id: "string" !
                 -inspection_symetrie_faciale: "string" @< normal, [anything] >
                 -inspection_coloration_teguments: "string" @< normal, [anything] >
-                -inspection_autres: "string"
+                -inspection_autres: "arrayList"
                 -palpation_atm_douleur: "string" @< oui, no >
                 -palpation_atm_bruite_articulaire: "string" @< claquement, craquement, crepitation, non >
                 -palpation_atm_jeu_condyliene: "string" @< ressaut, asymetrique, symetrique, subluxation, [anything] >
@@ -45,10 +45,10 @@ export default [
                 -palpation_chaine_ganglionaire_nombre: "integer"
                 -palpation_chaine_ganglionaire_taille: "string"
                 -palpation_chaine_ganglionaire_tempirature: "string" @< chaude, froide >
-                -palpation_muscles_masseter: "string" @< isotonique, hypertrophique, douleure, [anything] >
-                -palpation_muscles_temporal: "string" @< isotonique, hypertrophique, douleure, [anything] >
-                -palpation_muscles_pterygoidien_interne: "string" @< isotonique, hypertrophique, douleure, [anything] >
-                -palpation_muscles_pterygoidien_externe: "string" @< isotonique, hypertrophique, douleure, [anything] >
+                -palpation_muscles_masseter: "arrayList" @< isotonique, hypertrophique, douleure, [anything] >
+                -palpation_muscles_temporal: "arrayList" @< isotonique, hypertrophique, douleure, [anything] >
+                -palpation_muscles_pterygoidien_interne: "arrayList" @< isotonique, hypertrophique, douleure, [anything] >
+                -palpation_muscles_pterygoidien_externe: "arrayList" @< isotonique, hypertrophique, douleure, [anything] >
             )      
         `,
         response: ``,
@@ -68,8 +68,7 @@ export default [
                 -patient_id: "string" !
                 -motif_consultation: "arrayList" ! @< fonctionnele, esthetique, douleure, [anything] >
                 -antecedents allergique: "arrayList" ! @< >
-                -mauvaise_habitude: "arrayList",
-                -fumeur_bruxisme_onychophagie: "arrayList" @< succion, digigtale, [anything] >
+                -mauvaise_habitude: "arrayList" @< fumeur, bruxisme, onychophagie, succion, digigtale, [anything] >
                 -AG_personnel_maladie: "arrayList" ! @< non, [anything] >
                 -AG_personnel_traitement: "arrayList" ! @< non, [anything] >
                 -AG_mere_maladie: "arrayList"
