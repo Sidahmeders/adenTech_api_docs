d0c83917688287a96fcafd92b6bfa3e185b1bce7
Sidahmeders <ahorob57@gmail.com>
2021-09-12 00:39:07 +0200

    added get patients route

--- models/patients.js
+++ models/patients.js
@@ -53,5 +53,48 @@ export default [
                 this options are meant to allow only the selected service
                 to access and see what is in the patient document.
         `
+    },
+    {
+        title: 'return/get all the user patients',
+        method: 'GET',
+        route: '/patients',
+        response: `
+            {
+                -data: {
+                    --patients: [
+                        ---{
+                            ----_id: "string"
+                            ----user_id: "string" !
+                            ----first_name: "string" !
+                            ----last_name: "string" !
+                            ----gender: "string" !
+                            ----age: "integer" !
+                            ----address: "string"
+                            ----phone_number: "integer"
+                            ----email: "string"
+                            ----marital_status: "string"
+                            ----job: "string"
+                            ----specialty_access: "arrayList"
+                            ----appointment: "date"
+                            ----date_of_modification: "date"
+                            ----date_of_creation: "date"
+                            ----__v: "integer"
+                        ---},
+                        ---{ .... }
+                    --]
+                -}
+            }
+        `,
+        headers: `
+        *Request Headers*
+            -authToken: "HS256_bearer_token" !
+            -accept: "application/json"
+            -contentType: "application/json"
+        `,
+        specification: `
+            *specialty_access*
+                this options are meant to allow only the selected service
+                to access and see what is in the patient document.
+        `
     }
 ]
